336a9133760319df20879a2be3712aba130b2b71
Favio95 <64169923+Favio95@users.noreply.github.com>
2020-05-06 12:29:53 -0600

    cambio css
    los link de css se llaman el el header y footer

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,6 @@
 <?php
 
-
-
+include '../global/conexion.php';
 $buscar_carrito = $pdo->prepare("SELECT * FROM Carrito c INNER JOIN Clientes cli
                                     ON c.FK_Cliente = cli.PK_CLiente INNER JOIN Usuarios u
                                     ON cli.FK_Usuario = u.PK_Usuario 
@@ -13,7 +12,24 @@ $carrito = $buscar_carrito->fetchAll(PDO::FETCH_ASSOC);
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Shoppingapp</title>
 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link href="../static/css/styles.css" rel="stylesheet" type="text/css" media="all" />
+
+  <script src="https://kit.fontawesome.com/b2dbb6a24d.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+ 
+</head>
+<body>
 <!-- primer nav -->
 <nav class="navbar navbar-expand-lg navbar-light">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
@@ -32,21 +48,21 @@ $carrito = $buscar_carrito->fetchAll(PDO::FETCH_ASSOC);
         <a class="nav-link border-right" href="#"><i class="fas fa-phone mr-2"></i>2781 0000</a>
 			</li>
 			<li class="col-md-2 nav-item">
-        <a class="nav-link border-right" href="./login.php"><i class="fas fa-sign-in-alt mr-2"></i>Log In</a>
+        <a class="nav-link border-right" href="/shoppingapp/Login/"><i class="fas fa-sign-in-alt mr-2"></i>Log In</a>
 			</li>
 			<li class="col-md-2 nav-item">
-        <a class="nav-link border-right" href="./registro_usuario.php"><i class="fas fa-sign-out-alt mr-2"></i>Registrarse</a>
+        <a class="nav-link border-right" href="/shoppingapp/Registro-Usuario/"><i class="fas fa-sign-out-alt mr-2"></i>Registrarse</a>
 			</li>
 			<li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Configuración
         </a>
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-					<form action="./registro_datos.php" method="POST">
+					<form action="/shoppingapp/Admin/" method="POST">
 						<input type="hidden" name="menu" value="registro_categoria" />
 						<a class="dropdown-item" href="#" value="category" name="menu" onclick="this.parentNode.submit()" >Gestión de categoría</a>
           </form>
-          <form action="./login.php" method="POST">
+          <form action="/shoppingapp/Login/" method="POST">
 						<input type="hidden" name="sesion" value="cerrar" />
 						<a class="dropdown-item" href="#" value="salir" name="menu" onclick="this.parentNode.submit()" >Salir</a>
 					</form>
@@ -63,7 +79,7 @@ $carrito = $buscar_carrito->fetchAll(PDO::FETCH_ASSOC);
 <!-- segundo nav -->
 <div class="row" style="width:100%;">
 	<div class="col-md-4">
-		<a href="./home.php"><div class="col-md-10 offset-md-1" id="logo1" alt=""></div></a>
+		<a href="/shoppingapp/Home/"><div class="col-md-10 offset-md-1" id="logo1" alt=""></div></a>
 	</div>
 	<div class="col-md-6" >
 			<form class="form-inline" id="search-form" action="#" method="post">
